55c28df331f820a1cf82c29be3da49c1752a188b
Joseph D Hughes <jdhughes@usgs.gov>
2017-12-14 12:55:07 -0600

    Don't run large models on travis because of 50 min maximum run time.

--- doc/version.py
+++ doc/version.py
@@ -1,11 +1,11 @@
 # MODFLOW 6 version file automatically created using...pre-commit.py
-# created on...December 14, 2017 07:54:52
+# created on...December 14, 2017 12:55:08
 
 major = 6
 minor = 0
 micro = 1
-build = 9
-commit = 15
+build = 10
+commit = 16
 
 __version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
 __build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
